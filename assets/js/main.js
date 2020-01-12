c270cbc239d8965854788dec7247a678a29b7bbd
miguedesign <migue.nodos@gmail.com>
2020-01-12 13:17:31 -0600

    first commit with files

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*!
+	Theme Name: Migue
+	Theme URI: http://migue.design
+	Author: @elnodos
+	Author URI: http://migue.design
+	Author Contact: hello@migue.design
+	Version: 2
+*/
+
+jQuery(document).ready(function($) {
+
+
+
+});
+
+
+
+
+
