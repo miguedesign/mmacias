e3f783a4537270d71fc0167c8612281345daa14f
migue.nodos@gmail.com <migue.nodos@gmail.com>
2020-01-19 21:00:44 -0600

    computacion = detalle-curso

--- views/_footer.php
+++ views/_footer.php
@@ -20,6 +20,7 @@
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+<script type='text/javascript' src="./assets/js/owl.carousel.min.js" async></script>
 <script type='text/javascript' src="./assets/js/main.js" async></script>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
